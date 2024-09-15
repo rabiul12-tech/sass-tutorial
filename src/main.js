b6aa1191b074bcc339fea0c0e1e980f917d2f427
rabiul alam <rabiulalamin@gamil.com>
2024-09-15 06:52:15 +0600

    sass tutorial

---
+++ src/main.js
@@ -0,0 +1,115 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".header__content .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".header__content h1", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".header__content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".header__btns", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+
+ScrollReveal().reveal(".service__card", {
+  duration: 1000,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".destination__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".trip__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+ScrollReveal().reveal(".trip__content .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".trip__content .section__header", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".trip__list li", {
+  ...scrollRevealOption,
+  delay: 1500,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".client__content .section__subheader", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".client__content .section__header", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+const swiper = new Swiper(".swiper", {
+  direction: "vertical",
+  autoHeight: true,
+  slidesPerView: 1,
+});
+function toggleAnswer(element) {
+  const allAnswers = document.querySelectorAll(".faq-answer");
+  const allButtons = document.querySelectorAll(".faq-button");
+  const allQuestions = document.querySelectorAll(".faq-question");
+
+  allAnswers.forEach((answer) => {
+    if (answer !== element.nextElementSibling) {
+      answer.classList.remove("show");
+    }
+  });
+
+  allButtons.forEach((button) => {
+    if (button !== element.querySelector(".faq-button")) {
+      button.classList.remove("rotate");
+    }
+  });
+
+  allQuestions.forEach((question) => {
+    if (question !== element) {
+      question.classList.remove("active");
+    }
+  });
+
+  const answer = element.nextElementSibling;
+  const button = element.querySelector(".faq-button");
+  const isAnswerVisible = answer.classList.contains("show");
+
+  answer.classList.toggle("show", !isAnswerVisible);
+  button.classList.toggle("rotate", !isAnswerVisible);
+  element.classList.toggle("active", !isAnswerVisible);
+}
